1091181ea2c7f9ee79bf6440c9f0b1299f1bb2f0
asainingen <116788973+asainingen@users.noreply.github.com>
2025-01-31 20:52:44 +0900

    ハンバーガーメニューが開いた際に全体にブラーをかける

--- javascript/sidebar-toggle.js
+++ javascript/sidebar-toggle.js
@@ -2,14 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar-mobile");
   const toggleButton = document.getElementById("sidebar-toggle");
   const overlay = document.querySelector(".overlay");
+  const container = document.querySelector(".container");
 
   // トグルボタンのクリックイベント
   toggleButton.addEventListener("click", function () {
     sidebar.classList.toggle("active");
     if (sidebar.classList.contains("active")) {
       overlay.style.display = "block";
+      container.style.filter = "blur(2px)";
     } else {
       overlay.style.display = "none";
+      container.style.filter = "blur(0.2px)";
     }
   });
 
@@ -17,5 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
   overlay.addEventListener("click", function () {
     sidebar.classList.remove("active");
     overlay.style.display = "none";
+    container.style.filter = "blur(0.2px)";
   });
 });
