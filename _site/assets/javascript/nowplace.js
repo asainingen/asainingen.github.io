76db4ad46fe8db42056d00927d91db7c67adb0cd
asainingen <116788973+asainingen@users.noreply.github.com>
2025-02-02 20:27:52 +0900

    左サイドバーの現在のページの色を反転

--- _site/assets/javascript/nowplace.js
+++ _site/assets/javascript/nowplace.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".sidebar a");
+  if (navLinks.length > 0) {
+    for (let navLink of navLinks) {
+      if (navLink.getAttribute("href") == path) {
+        console.log(navLink);
+        console.log(path);
+        navLink.classList.add("nowpage");
+      }
+    }
+  }
+});
